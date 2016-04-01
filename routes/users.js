a79ea811a3399781b601565bd3d5eaac04d98793
Nikita Galkin <nikita@galk.in>
2016-04-01 14:55:46 +0300

    Move raml to spec folder

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var usersList = [];
+usersList.push({firstName: 'Alica', lastName: 'Smith'});
+usersList.push({firstName: 'Neo', lastName: 'Chosen One'});
+
 router.get('/', function(req, res, next) {
-    const usersList = ['alica'];
     res.status(200).json(usersList);
 });
 
-router.get('/alica/', function(req, res, next) {
-    const user = {firstName: 'Alica', lastName: 'Smith'};
-    res.status(200).json(user);
-});
-
-router.get('/bob/', function(req, res, next) {
+router.get('/:id/', function(req, res, next) {
+    var user = usersList[req.params.id];
+    if (user) {
+        return res.status(200).json(user);
+    }
     res.sendStatus(404);
 });
+
 module.exports = router;
--- test/abao/hooks.js
+++ test/abao/hooks.js
@@ -1,11 +1,11 @@
 var hooks = require('hooks');
 
 hooks.before('GET /users/{userId} -> 200', function(test, done) {
-    test.request.params = {userId: 'alica'};
+    test.request.params = {userId: '0'};
     done();
 });
 
 hooks.before('GET /users/{userId} -> 404', function(test, done) {
-    test.request.params = {userId: 'bob'};
+    test.request.params = {userId: '123412'};
     done();
 });
